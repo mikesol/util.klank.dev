6be06e2420375fcdf5121a94ea93bafdd39db278
Mike Solomon <mike@meeshkan.com>
2020-12-28 09:54:07 +0200

    Adds canvas builder

--- src/Klank/Dev/Util.purs
+++ src/Klank/Dev/Util.purs
@@ -5,6 +5,8 @@ import Control.Promise (toAffE)
 import Data.Array (filter)
 import Data.Either (Either(..), either)
 import Data.Lens (_2, over)
+import Data.List (List)
+import Data.Map as M
 import Data.Maybe (Maybe(..))
 import Data.Traversable (sequence)
 import Data.Tuple (Tuple(..), fst)
@@ -15,12 +17,16 @@ import Effect.Exception (Error)
 import FRP.Behavior.Audio (BrowserAudioBuffer, decodeAudioDataFromUri)
 import Foreign.Object (Object, fromHomogeneous)
 import Foreign.Object as O
-import Type.Klank.Dev (Buffers, Images, Videos)
+import Graphics.Canvas (CanvasElement, TextMetrics, getContext2D)
+import Graphics.Painting (MeasurableText, Painting, render, measurableTextToMetrics)
+import Type.Klank.Dev (Buffers, Images, Videos, Canvases)
 import Type.Row.Homogeneous (class Homogeneous)
+import Unsafe.Coerce (unsafeCoerce)
 import Web.DOM.Document (createElement)
 import Web.Event.Event (EventType(..))
 import Web.Event.EventTarget (addEventListener, eventListener)
-import Web.HTML (HTMLImageElement, HTMLVideoElement, window)
+import Web.HTML (window)
+import Web.HTML.HTMLCanvasElement as HTMLCanvasElement
 import Web.HTML.HTMLDocument (toDocument)
 import Web.HTML.HTMLImageElement as HTMLImageElement
 import Web.HTML.HTMLMediaElement as HTMLMediaElement
@@ -33,7 +39,7 @@ affize f =
     f (cb <<< Right) (cb <<< Left)
     pure mempty
 
-fetchVideo :: String -> Aff HTMLVideoElement
+fetchVideo :: String -> Aff HTMLVideoElement.HTMLVideoElement
 fetchVideo str =
   affize \res rej -> do
     document <- (toDocument <$> (document =<< window))
@@ -58,7 +64,29 @@ fetchVideo str =
       (HTMLVideoElement.toEventTarget video)
     HTMLMediaElement.setSrc str (HTMLVideoElement.toHTMLMediaElement video)
 
-fetchImage :: String -> Aff HTMLImageElement
+fetchCanvas :: CanvasInfo -> Aff HTMLCanvasElement.HTMLCanvasElement
+fetchCanvas ci = do
+  images <- affize \res rej -> makeImagesKeepingCache 20 ci.images O.empty res rej
+  videos <- affize \res rej -> makeVideosKeepingCache 20 ci.videos O.empty res rej
+  liftEffect do
+    document <- (toDocument <$> (document =<< window))
+    node <- HTMLCanvasElement.fromElement <$> (createElement "canvas" document)
+    canvas <- case node of
+      Nothing -> throwError (error "Could not convert to canvas node")
+      Just x -> pure x
+    canvasCtx <- getContext2D $ htmlCanvasElementToCanvasElement canvas
+    words <- measurableTextToMetrics canvasCtx ci.words
+    let
+      painting = ci.painting { words }
+    render canvasCtx
+      { canvases: O.empty
+      , images
+      , videos
+      }
+      painting
+    pure canvas
+
+fetchImage :: String -> Aff HTMLImageElement.HTMLImageElement
 fetchImage str =
   affize \res rej -> do
     image <- HTMLImageElement.create unit
@@ -79,7 +107,7 @@ fetchImage str =
       (HTMLImageElement.toEventTarget image)
     HTMLImageElement.setSrc str image
 
-loopDownload :: forall a. (String -> Aff a) -> Int -> String -> Aff a
+loopDownload :: forall a b. (b -> Aff a) -> Int -> b -> Aff a
 loopDownload f maxAttempts str = go 0
   where
   go nt =
@@ -87,7 +115,7 @@ loopDownload f maxAttempts str = go 0
       >>= either
           ( \e ->
               if nt > maxAttempts then
-                throwError (error $ "Max download attempts reached for " <> str)
+                throwError (error $ "Max fetching attempts reached for asset.")
               else do
                 delay (Milliseconds 20.0)
                 go (nt + 1)
@@ -96,7 +124,7 @@ loopDownload f maxAttempts str = go 0
     where
     res = try $ f str
 
-makeSomethingUsingCache :: forall a. (String -> Aff a) -> Int -> (O.Object a -> Tuple (Array (Tuple String String)) (O.Object a)) -> O.Object a -> (O.Object a -> Effect Unit) -> (Error -> Effect Unit) -> Effect Unit
+makeSomethingUsingCache :: forall a b. (b -> Aff a) -> Int -> (O.Object a -> Tuple (Array (Tuple String b)) (O.Object a)) -> O.Object a -> (O.Object a -> Effect Unit) -> (Error -> Effect Unit) -> Effect Unit
 makeSomethingUsingCache looper maxAttempts bf prev' =
   affable do
     sequential
@@ -114,26 +142,42 @@ makeSomethingUsingCache looper maxAttempts bf prev' =
   where
   (Tuple newB prev) = bf prev'
 
-type CacheFunction a
-  = O.Object a -> Tuple (Array (Tuple String String)) (O.Object a)
+type CacheFunction a b
+  = O.Object a -> Tuple (Array (Tuple String b)) (O.Object a)
 
-makeBuffersUsingCache :: Int -> CacheFunction BrowserAudioBuffer -> Buffers
+makeBuffersUsingCache :: Int -> CacheFunction BrowserAudioBuffer String -> Buffers
 makeBuffersUsingCache maxAttempts bf ctx = makeSomethingUsingCache (toAffE <<< decodeAudioDataFromUri ctx) maxAttempts bf
 
 makeBuffersKeepingCache :: Int -> Array (Tuple String String) -> Buffers
 makeBuffersKeepingCache maxAttempts = (makeBuffersUsingCache maxAttempts) <<< Tuple
 
-makeImagesUsingCache :: Int -> CacheFunction HTMLImageElement -> Images
+makeImagesUsingCache :: Int -> CacheFunction HTMLImageElement.HTMLImageElement String -> Images
 makeImagesUsingCache = makeSomethingUsingCache fetchImage
 
 makeImagesKeepingCache :: Int -> Array (Tuple String String) -> Images
 makeImagesKeepingCache maxAttempts = (makeImagesUsingCache maxAttempts) <<< Tuple
 
-makeVideosUsingCache :: Int -> CacheFunction HTMLVideoElement -> Videos
+makeVideosUsingCache :: Int -> CacheFunction HTMLVideoElement.HTMLVideoElement String -> Videos
 makeVideosUsingCache = makeSomethingUsingCache fetchVideo
 
 makeVideosKeepingCache :: Int -> Array (Tuple String String) -> Videos
 makeVideosKeepingCache maxAttempts = (makeVideosUsingCache maxAttempts) <<< Tuple
+
+makeCanvasesUsingCache :: Int -> CacheFunction HTMLCanvasElement.HTMLCanvasElement CanvasInfo -> Canvases
+makeCanvasesUsingCache = makeSomethingUsingCache fetchCanvas
+
+makeCanvasesKeepingCache :: Int -> Array (Tuple String CanvasInfo) -> Canvases
+makeCanvasesKeepingCache maxAttempts = (makeCanvasesUsingCache maxAttempts) <<< Tuple
+
+type CanvasInfo
+  = { images :: Array (Tuple String String)
+    , videos :: Array (Tuple String String)
+    , painting :: { words :: M.Map MeasurableText TextMetrics } -> Painting
+    , words :: List MeasurableText
+    }
+
+htmlCanvasElementToCanvasElement :: HTMLCanvasElement.HTMLCanvasElement -> CanvasElement
+htmlCanvasElementToCanvasElement = unsafeCoerce
 
 affableRec ::
   forall (a :: # Type) b.
